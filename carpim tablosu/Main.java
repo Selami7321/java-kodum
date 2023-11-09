11d68a16c03da04f103ad79707f2c85f34c97985
Selami7321 <116799597+Selami7321@users.noreply.github.com>
2023-11-09 13:37:07 +0300

    Add files via upload

---
+++ carpim tablosu/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+   for(int i=1;i<=10;i++){
+       for(int k=1;k<=10;k++){
+           System.out.print(( i*k) + "\t");
+       }
+       System.out.println();
+   }
+
+
+
+    }
+}
